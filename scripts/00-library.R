08e54b424fb9da60950553c1628dcfb811e599b4
VictorSibinelli <victor.sibinelli@usp.br>
2024-07-15 18:30:29 -0300

    adding package version control to library script

--- scripts/00-library.R
+++ scripts/00-library.R
@@ -2,11 +2,11 @@
 #
 #Victor Sibinelli (victor.sibinelli@usp.br / sibinelli95@gmail.com)
 #13/07/2024
-# Scrpt 00 - Instaling and loading packages needed in the analysis
+# Scrpt 00 - Instaling and loading packages needed in the analysis + packages version control
 #################################################################
  
 # Package names
-packages <- c("tidyverse", "here", "plotly", "data.table", "readr")
+packages <- c("tidyverse", "here", "plotly", "data.table", "readr" , "groundhog")
 
 # Install packages not yet installed
 installed_packages <- packages %in% rownames(installed.packages())
@@ -16,3 +16,8 @@ if (any(installed_packages == FALSE)) {
 
 # Load packages
 invisible(lapply(packages, library, character.only = TRUE))
+
+#########################################################################
+# If you desire to run the analysis with the same package versions, remove the comments in the lines below and run it
+
+#groundhog.library(packages, "2024-07-01")
