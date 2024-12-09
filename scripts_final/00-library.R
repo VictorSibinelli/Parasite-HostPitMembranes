f59f3eff323a2b8ddd63d76cfd4d4d90045ca263
VictorSibinelli <victor.sibinelli@usp.br>
2024-12-09 14:28:15 -0300

    adding Populus nigra 1 data

---
+++ scripts_final/00-library.R
@@ -0,0 +1,35 @@
+######################################################################
+#
+# Victor Sibinelli (victor.sibinelli@usp.br / sibinelli95@gmail.com)
+# 13/07/2024
+# Script 00 - Installing and loading packages needed in the analysis
+#           + packages version control
+######################################################################
+
+# Package names
+packages <- c(
+  "tidyverse", "here", "plotly", "data.table",
+  "groundhog", "car", "htmlwidgets","DHARMa",
+  "lattice", "lme4", "nlme", "predictmeans", "performance"
+)
+
+# Install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+# Load packages
+invisible(lapply(packages, library, character.only = TRUE))
+print("Packages Loaded")
+
+#########################################################################
+# If you desire to run the analysis with the same package versions,
+# remove the comments in the lines below and run it
+
+# groundhog.library(packages, "2024-07-01")
+
+rm(list = ls())
+
+
+
